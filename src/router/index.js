7409386a6625c6b837e9bbdef8f7ecc1dc33f932
zzm <1054965228@qq.com>
2020-06-30 21:24:36 +0800

    导入Vant组件库

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import useVant from '../views/Vant/useVant.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/useVant',
+    component: useVant
+  }
 ]
 
 const router = new VueRouter({
