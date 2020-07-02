1bce00d13586ad50f5a23075f405b48cd774e930
water-zm <l>
2020-07-02 21:23:42 +0800

    完成home页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import VueRouter from 'vue-router'
 import useVant from '../views/Vant/useVant.vue'
 import login from '../views/login/login.vue'
 import home from '../views/home/home.vue'
+import index from '../views/home/index/index.vue'
+import question from '../views/home/question/question.vue'
+import video from '../views/home/video/video.vue'
+import my from '../views/home/my/my.vue'
 
 Vue.use(VueRouter)
 
@@ -21,7 +25,14 @@ const routes = [
   },
   {
     path: '/home',
-    component: home
+    component: home,
+    redirect: '/index',
+    children: [
+      { path: '/index', component: index },
+      { path: '/question', component: question },
+      { path: '/video', component: video },
+      { path: '/my', component: my }
+    ]
   }
 ]
 
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    home
+    <router-view></router-view>
+    <van-tabbar route>
+        <van-tabbar-item to="/index" icon="home-o">主页</van-tabbar-item>
+        <van-tabbar-item to="/question" icon="comment-o">问答</van-tabbar-item>
+        <van-tabbar-item to="/video" icon="tv-o">视频</van-tabbar-item>
+        <van-tabbar-item to="/my" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
