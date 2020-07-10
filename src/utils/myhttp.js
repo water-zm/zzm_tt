7c7cdb6ff0f5362c49a303e5a7802e41b59b8cc9
water-zm <l>
2020-07-10 23:40:22 +0800

    刷新token处理

--- src/utils/myhttp.js
+++ src/utils/myhttp.js
@@ -1,12 +1,17 @@
 import axios from 'axios'
 import store from '@/store/index.js'
 import jsonbigint from 'json-bigint'
+import { localSet } from '@/utils/mylocal'
 
 const instance = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn',
   transformResponse: [function (data) {
     return jsonbigint.parse(data)
   }]
+})
+
+const instanceToken = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn'
 })
 
 instance.interceptors.request.use(function (config) {
@@ -19,7 +24,26 @@ instance.interceptors.request.use(function (config) {
 
 instance.interceptors.response.use(function (response) {
   return response
-}, function (error) {
+}, async function (error) {
+  console.dir(error)
+  if (error.response.status === 401) {
+    const refToken = store.state.userInfo.refresh_token
+    const res = await instanceToken({
+      url: '/app/v1_0/authorizations',
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${refToken}`
+      }
+    })
+    const objToken = {
+      token: res.data.data.token,
+      refresh_token: refToken
+    }
+    console.log(res)
+    store.commit('setUserInfo', objToken)
+    localSet(objToken)
+    return instance(error.config)
+  }
   return Promise.reject(error)
 })
 
